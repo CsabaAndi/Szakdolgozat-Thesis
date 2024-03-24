079f311f7a0527b797d31c6202ba0d63340b2da7
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-03-24 16:31:27 +0100

    [Refactor] renamed read_input() --> read_all_fiels_in_dir() | new function read all csv files in test-data dir then converts it to pandas df

--- src/file_io.py
+++ src/file_io.py
@@ -1,11 +1,26 @@
 import pandas as pd
+import os 
+import glob 
+import stats
+  
+  
+def read_all_files_in_dir(): # TODO file path
+    table_type = "wide"
+    path = r"../test-data/"
+    csv_files = glob.glob(os.path.join(path, "*" + table_type + ".csv")) 
 
+    for f in csv_files: 
+        file_name = os.path.basename(f)
+        name_without_extension = os.path.splitext(file_name)
+        split_fn = name_without_extension[0].split('_')
+        
+        df = pd.read_csv(f, header=0, index_col=None, engine='python') 
+        stats.some_stat(df)
 
-def read_input(test_file_path):
-    data = pd.read_csv(test_file_path, sep=r'\t', header=0, index_col=None, engine='python') # index_col=0 | wide + ou
-    df = pd.DataFrame(data)
-    return df
-   
+        print(f"Country: {split_fn[0]} {'\n'}League: {split_fn[1]} {'\n'}Year: {split_fn[2]} {'\n'}Table_type: {split_fn[3]}")
+        print(df.to_string())
+        
+    
     
 if __name__ == "__main__":
     pass
